48d9fb4f732c94871bcf0001eacdf9390c9ac285
Simon Lyngshede <slyngshede@wikimedia.org>
2023-01-30 09:43:33 +0100

    Get attributes from user details

--- social_core/backends/cas.py
+++ social_core/backends/cas.py
@@ -29,34 +29,6 @@ class CASOpenIdConnectAuth(OpenIdConnectAuth):
     """
 
     name = "cas"
-    # Override OIDC_ENDPOINT in your subclass to enable autoconfig of OIDC
-    OIDC_ENDPOINT = None
-    ID_TOKEN_MAX_AGE = 600
-    DEFAULT_SCOPE = ["openid", "profile", "email"]
-    EXTRA_DATA = ["id_token", "refresh_token", ("sub", "id")]
-    REDIRECT_STATE = False
-    ACCESS_TOKEN_METHOD = "POST"
-    REVOKE_TOKEN_METHOD = "GET"
-    ID_KEY = "sub"
-    USERNAME_KEY = "preferred_username"
-    JWT_ALGORITHMS = ["RS256"]
-    JWT_DECODE_OPTIONS = dict()
-    # When these options are unspecified, server will choose via openid autoconfiguration
-    ID_TOKEN_ISSUER = ""
-    ACCESS_TOKEN_URL = ""
-    AUTHORIZATION_URL = ""
-    REVOKE_TOKEN_URL = ""
-    USERINFO_URL = ""
-    JWKS_URI = ""
-    TOKEN_ENDPOINT_AUTH_METHOD = ""
-
-    def oidc_endpoint(self):
-        logger.info(f'settings: {self.setting}')
-        return self.setting("OIDC_ENDPOINT", self.OIDC_ENDPOINT)
-
-    @cache(ttl=86400)
-    def oidc_config(self):
-        return self.get_json(self.oidc_endpoint() + "/.well-known/openid-configuration")
 
     def user_data(self, access_token, *args, **kwargs):
         data = self.get_json(
@@ -68,14 +40,14 @@ class CASOpenIdConnectAuth(OpenIdConnectAuth):
 
     def get_user_details(self, response):
         username_key = self.setting("USERNAME_KEY", self.USERNAME_KEY)
-        attr = response.get('attributes', {})
+        attributes = self.user_data(response.get('access_token'))
 
-        logger.info(f'attributes: {attr}')
+        logger.info(f'attributes: {attributes}')
 
         return {
-            "username": attr.get(username_key),
-            "email": attr.get("email"),
-            "fullname": attr.get("name"),
-            "first_name": attr.get("given_name"),
-            "last_name": attr.get("family_name"),
+            "username": attributes.get(username_key),
+            "email": attributes.get("email"),
+            "fullname": attributes.get("name"),
+            "first_name": attributes.get("given_name"),
+            "last_name": attributes.get("family_name"),
         }
